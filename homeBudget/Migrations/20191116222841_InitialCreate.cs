a55f4b902d367a9c3377b325d79ee98acb3a528b
Matias Gonzalez Tognoni <megt_user@hotmail.com>
2019-11-16 23:29:40 +0100

    Add first migration

---
+++ homeBudget/Migrations/20191116222841_InitialCreate.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace homeBudget.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BankAccounts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    AccountNumber = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BankAccounts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SubCategories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    KeyWord = table.Column<string>(nullable: true),
+                    Category = table.Column<string>(nullable: true),
+                    SupPorject = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SubCategories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "KeeWords",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: false),
+                    XCoordinate = table.Column<double>(nullable: false),
+                    YCoordinate = table.Column<double>(nullable: false),
+                    SubCategoryId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_KeeWords", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_KeeWords_SubCategories_SubCategoryId",
+                        column: x => x.SubCategoryId,
+                        principalTable: "SubCategories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Transactions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    DateTime = table.Column<DateTime>(nullable: false),
+                    Text = table.Column<string>(nullable: true),
+                    Amount = table.Column<double>(nullable: false),
+                    BankAccountId = table.Column<int>(nullable: true),
+                    KeeWordId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transactions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Transactions_BankAccounts_BankAccountId",
+                        column: x => x.BankAccountId,
+                        principalTable: "BankAccounts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Transactions_KeeWords_KeeWordId",
+                        column: x => x.KeeWordId,
+                        principalTable: "KeeWords",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TransactionsKeewordses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Description = table.Column<string>(nullable: true),
+                    KeeWordId = table.Column<int>(nullable: false),
+                    TransactionId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TransactionsKeewordses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TransactionsKeewordses_KeeWords_KeeWordId",
+                        column: x => x.KeeWordId,
+                        principalTable: "KeeWords",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TransactionsKeewordses_Transactions_TransactionId",
+                        column: x => x.TransactionId,
+                        principalTable: "Transactions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_KeeWords_SubCategoryId",
+                table: "KeeWords",
+                column: "SubCategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_BankAccountId",
+                table: "Transactions",
+                column: "BankAccountId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_KeeWordId",
+                table: "Transactions",
+                column: "KeeWordId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TransactionsKeewordses_KeeWordId",
+                table: "TransactionsKeewordses",
+                column: "KeeWordId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TransactionsKeewordses_TransactionId",
+                table: "TransactionsKeewordses",
+                column: "TransactionId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TransactionsKeewordses");
+
+            migrationBuilder.DropTable(
+                name: "Transactions");
+
+            migrationBuilder.DropTable(
+                name: "BankAccounts");
+
+            migrationBuilder.DropTable(
+                name: "KeeWords");
+
+            migrationBuilder.DropTable(
+                name: "SubCategories");
+        }
+    }
+}
